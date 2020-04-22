5955a7b885a282c32eb8f35fe9da2d66f5dbf98b
Luka S <lukastan93@gmail.com>
2020-04-22 16:21:01 +0400

    First commit, front-end

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a , b){
+    return a + b;
+}
+
+function subtract(a , b){
+    return a - b;
+}
+
+function multiply(a , b){
+    return a * b;
+}
+
+function divide(a , b){
+    return a / b;
+}
+
+function operate(operand1 , operand2 , operator){
+    switch (operator){
+        case '+':
+            return add (operand1, operand2);
+            break;
+        case '-':
+            return subtract(operand1, operand2);
+            break;
+        case '*':
+            return multiply(operand1, operand2);
+            break;
+        case '/':
+            return divide(operand1, operand2);
+            break;
+    }
+}
