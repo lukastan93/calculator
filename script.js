7f63514280a77d6a7e070c29791b4b9443998f54
Luka S <lukastan93@gmail.com>
2020-04-22 20:09:17 +0400

    cleaned up code with event listeners

--- script.js
+++ script.js
@@ -2,56 +2,20 @@ const display = document.querySelector('.display');
 let operand1 = null;
 let operand2 = null;
 let operator = null;
+let equalsPressed = 0;
 
-const zeroButton = document.querySelector('.zero').onclick = pressZero;
-function pressZero(){
-    display.textContent += '0';
-}
-
-const oneButton = document.querySelector('.one').onclick = pressOne;
-function pressOne(){
-    display.textContent += '1';
-}
-
-const twoButton = document.querySelector('.two').onclick = pressTwo;
-function pressTwo(){
-    display.textContent += '2';
-}
-
-const threeButton = document.querySelector('.three').onclick = pressThree;
-function pressThree(){
-    display.textContent += '3';
-}
-
-const fourButton = document.querySelector('.four').onclick = pressFour;
-function pressFour(){
-    display.textContent += '4';
-}
-
-const fiveButton = document.querySelector('.five').onclick = pressFive;
-function pressFive(){
-    display.textContent += '5';
-}
-
-const sixButton = document.querySelector('.six').onclick = pressSix;
-function pressSix(){
-    display.textContent += '6';
-}
-
-const sevenButton = document.querySelector('.seven').onclick = pressSeven;
-function pressSeven(){
-    display.textContent += '7';
-}
-
-const eightButton = document.querySelector('.eight').onclick = pressEight;
-function pressEight(){
-    display.textContent += '8';
-}
-
-const nineButton = document.querySelector('.nine').onclick = pressNine;
-function pressNine(){
-    display.textContent += '9';
-}
+let buttons = document.querySelectorAll('.number-button');
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        if (equalsPressed === 0){
+            display.textContent += event.target.innerHTML;
+        }
+        else{
+            display.textContent = event.target.innerHTML;
+            equalsPressed = 0;
+        }
+    })
+})
 
 const clearButton = document.querySelector('.clear-button').onclick = clearAll;
 function clearAll(){
@@ -68,141 +32,78 @@ function clearDisplay(){
 const divideButton = document.querySelector('.divide').onclick = pressDivide;
 function pressDivide(){
     let displayValue = parseInt(display.textContent, 10);
-    switch (operator){
-        case "divide":
-            if(operand1 && display.textContent){
-                operand1 = divide(operand1 , displayValue);
-            }
-            else{operand1 = parseInt(display.textContent, 10);}
-            break;
-        case "multiply":
-            operand1 = multiply(operand1, displayValue);
-            break;
-        case "subtract":
-            operand1 = subtract(operand1, displayValue);
-            break;
-        case "add":
-            operand1 = add(operand1, displayValue);
-            break;
-        default:
-            operand1 = displayValue;
-            break;
+    if (operator === "-" || operator === "+" || operator === "*"){
+        operand1 = operate(operand1 , displayValue, operator);
     }
-    operator = 'divide';
+    else{
+        if(operand1 && display.textContent){
+            operand1 = divide(operand1 , displayValue);
+        }
+        else operand1 = parseInt(display.textContent, 10);
+    }
+    operator = '/';
     clearDisplay();
 }
 
-const multipllylButton = document.querySelector('.multiply').onclick = pressMultiply;
+const multiplyButton = document.querySelector('.multiply').onclick = pressMultiply;
 function pressMultiply(){
     let displayValue = parseInt(display.textContent, 10);
-    switch (operator){
-        case "multiply":
-            if(operand1 && display.textContent){
-                operand1 = multiply(operand1 , displayValue);
-            }
-            else{operand1 = parseInt(display.textContent, 10);}
-            break;
-        case "divide":
-            operand1 = divide(operand1, displayValue);
-            break;
-        case "subtract":
-            operand1 = subtract(operand1, displayValue);
-            break;
-        case "add":
-            operand1 = add(operand1, displayValue);
-            break;
-        default:
-            operand1 = displayValue;
-            break;
+    if (operator === "-" || operator === "/" || operator === "+"){
+        operand1 = operate(operand1 , displayValue, operator);
     }
-    operator = 'multiply';
+    else{
+        if(operand1 && display.textContent){
+            operand1 = multiply(operand1 , displayValue);
+        }
+        else operand1 = parseInt(display.textContent, 10);
+    }
+    operator = '*';
     clearDisplay();
 }
 
 const subtractButton = document.querySelector('.subtract').onclick = pressSubtract;
 function pressSubtract(){
     let displayValue = parseInt(display.textContent, 10);
-    switch (operator){
-        case "subtract":
-            if(operand1 && display.textContent){
-                operand1 = subtract(operand1 , displayValue);
-            }
-            else{operand1 = parseInt(display.textContent, 10);}
-            break;
-        case "multiply":
-            operand1 = multiply(operand1, displayValue);
-            break;
-        case "divide":
-            operand1 = divide(operand1, displayValue);
-            break;
-        case "add":
-            operand1 = add(operand1, displayValue);
-            break;
-        default:
-            operand1 = displayValue;
-            break;
+    if (operator === "+" || operator === "/" || operator === "*"){
+        operand1 = operate(operand1 , displayValue, operator);
     }
-    operator = 'subtract';
+    else{
+        if(operand1 && display.textContent){
+            operand1 = subtract(operand1 , displayValue);
+        }
+        else operand1 = parseInt(display.textContent, 10);
+    }
+    operator = '-';
     clearDisplay();
 }
 
 const addButton = document.querySelector('.add').onclick = pressAdd;
 function pressAdd(){
     let displayValue = parseInt(display.textContent, 10);
-    switch (operator){
-        case "add":
-            if(operand1 && display.textContent){
-                operand1 = add(operand1 , displayValue);
-            }
-            else{operand1 = parseInt(display.textContent, 10);}
-            break;
-        case "multiply":
-            operand1 = multiply(operand1, displayValue);
-            break;
-        case "subtract":
-            operand1 = subtract(operand1, displayValue);
-            break;
-        case "divide":
-            operand1 = divide(operand1, displayValue);
-            break;
-        default:
-            operand1 = displayValue;
-            break;
+    if (operator === "-" || operator === "/" || operator === "*"){
+        operand1 = operate(operand1 , displayValue, operator);
     }
-    operator = 'add';
+    else{
+        if(operand1 && display.textContent){
+            operand1 = add(operand1 , displayValue);
+        }
+        else operand1 = parseInt(display.textContent, 10);
+    }
+    operator = '+';
     clearDisplay();
 }
 
 const equalsButton = document.querySelector('.equals').onclick = pressEquals;
 function pressEquals(){
     operand2 = parseInt(display.textContent, 10);
-    switch (operator){
-        case 'divide':
-            display.textContent = divide(operand1 , operand2);
-            operand1 = null;
-            operand2 = null;
-            operator = null;
-            break;
-        case 'multiply':
-            display.textContent = multiply(operand1 , operand2);
-            operand1 = null;
-            operand2 = null;
-            operator = null;
-            break;
-        case 'subtract':
-            display.textContent = subtract(operand1 , operand2);
-            operand1 = null;
-            operand2 = null;
-            operator = null;
-            break;
-        case 'add':
-            display.textContent = add(operand1 , operand2);
-            operand1 = null;
-            operand2 = null;
-            operator = null;
-            break;
-        default: break;
+    if (operand1){
+    display.textContent = operate(operand1 , operand2, operator);
     }
+    else display.textContent = `${operand2}`
+    operand1 = null;
+    operand2 = null;
+    operator = null;
+    equalsPressed = 1;
 }
 
 function add(a , b){
@@ -218,22 +119,22 @@ function multiply(a , b){
 }
 
 function divide(a , b){
-    return a / b;
+    return((b === 0) ? 'DONKEY' : (a / b));
 }
 
-function operate(operand1 , operand2 , operator){
-    switch (operator){
+function operate(a , b , mod){
+    switch (mod){
         case '+':
-            return add (operand1, operand2);
+            return add (a, b);
             break;
         case '-':
-            return subtract(operand1, operand2);
+            return subtract(a, b);
             break;
         case '*':
-            return multiply(operand1, operand2);
+            return multiply(a, b);
             break;
         case '/':
-            return divide(operand1, operand2);
+            return divide(a, b);
             break;
     }
 }
